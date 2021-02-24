fd1dd13db1995849ded4f5a4d02e18d8b3a32a67
Bart Kalanski <bkalanski@gmail.com>
2021-02-24 17:12:02 +0000

    Add user registration screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
 
 const App = () => {
   return (
@@ -15,10 +16,11 @@ const App = () => {
       <main className="py-3">
         <Container>
           <Switch>
-          <Route path='/' component={HomeScreen} exact/>
           <Route path='/login' component={LoginScreen}/>
+          <Route path='/register' component={RegisterScreen}/>
           <Route path='/product/:id' component={ProductScreen}/>
           <Route path='/cart/:id?' component={CartScreen}/>
+          <Route path='/' component={HomeScreen} exact/>
           </Switch>
         </Container>
       </main>
