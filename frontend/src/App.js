ddd3567ac9cf2f59049b4ce031052e02775edb88
Bart Kalanski <bkalanski@gmail.com>
2021-03-10 19:47:22 +0000

    Add payment screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 import ShippingScreen from "./screens/ShippingScreen";
+import PaymentScreen from './screens/PaymentScreen'
 
 const App = () => {
   return (
@@ -18,12 +19,13 @@ const App = () => {
       <main className="py-3">
         <Container>
           <Switch>
-          <Route path='/shipping' component={ShippingScreen}/>
           <Route path='/login' component={LoginScreen}/>
           <Route path='/register' component={RegisterScreen}/>
           <Route path='/profile' component={ProfileScreen}/>
           <Route path='/product/:id' component={ProductScreen}/>
           <Route path='/cart/:id?' component={CartScreen}/>
+          <Route path='/shipping' component={ShippingScreen}/>
+          <Route path='/payment' component={PaymentScreen}/>
           <Route path='/' component={HomeScreen} exact/>
           </Switch>
         </Container>
