d09ab00435fff3a4c268c39ebe01c69ae69d4743
Bart Kalanski <bkalanski@gmail.com>
2021-03-18 16:37:32 +0000

    Update order screen

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,6 +4,7 @@ import { addOrderItems, getOrderById, updateOrderToPaid } from '../controllers/o
 import { protect } from '../middleware/authMiddleware.js'
 
 router.route('/').post(protect, addOrderItems)
+router.route('/myorders').post(protect, getMyOrders)
 router.route('/:id').get(protect, getOrderById)
 router.route('/:id/pay').put(protect, updateOrderToPaid)
 
