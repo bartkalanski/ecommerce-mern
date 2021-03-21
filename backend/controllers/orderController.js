b8574838d7446ac6fad2608bf1773c32faa86b3a
Bart Kalanski <bkalanski@gmail.com>
2021-03-21 16:38:37 +0000

    Add UserListScreen component

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,6 +1,5 @@
-import asyncHandler from "express-async-handler";
-import Order from "../models/orderModel.js";
-
+import asyncHandler from 'express-async-handler'
+import Order from '../models/orderModel.js'
 // @desc    Create new order
 // @route   POST /api/orders
 // @access  Private
@@ -13,12 +12,11 @@ const addOrderItems = asyncHandler(async (req, res) => {
     taxPrice,
     shippingPrice,
     totalPrice,
-  } = req.body;
-
+  } = req.body
   if (orderItems && orderItems.length === 0) {
-    res.status(400);
-    throw new Error("No order items");
-    return;
+    res.status(400)
+    throw new Error('No order items')
+    return
   } else {
     const order = new Order({
       orderItems,
@@ -29,35 +27,31 @@ const addOrderItems = asyncHandler(async (req, res) => {
       taxPrice,
       shippingPrice,
       totalPrice,
-    });
-
-    const createdOrder = await order.save();
-
-    res.status(201).json(createdOrder);
+    })
+    const createdOrder = await order.save()
+    res.status(201).json(createdOrder)
   }
-});
-
+})
 // @desc    Get order by ID
 // @route   GET /api/orders/:id
 // @access  Private
 const getOrderById = asyncHandler(async (req, res) => {
-  const order = await (await Order.findById(req.params.id)).populate(
-    "user",
-    "name email"
-  );
+  const order = await Order.findById(req.params.id).populate(
+    'user',
+    'name email'
+  )
   if (order) {
-    res.json(order);
+    res.json(order)
   } else {
-    res.status(404);
-    throw new Error("Order not found");
+    res.status(404)
+    throw new Error('Order not found')
   }
-});
-
+})
 // @desc    Update order to paid
 // @route   GET /api/orders/:id/pay
 // @access  Private
 const updateOrderToPaid = asyncHandler(async (req, res) => {
-  const order = await (await Order.findById(req.params.id))
+  const order = await Order.findById(req.params.id)
   if (order) {
     order.isPaid = true
     order.paidAt = Date.now()
@@ -65,24 +59,56 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
       id: req.body.id,
       status: req.body.status,
       update_time: req.body.update_time,
-      email_address: req.body.payer.email_address
+      email_address: req.body.payer.email_address,
     }
+    const updatedOrder = await order.save()
+    res.json(updatedOrder)
+  } else {
+    res.status(404)
+    throw new Error('Order not found')
+  }
+})
+
+// @desc    Update order to delivered
+// @route   GET /api/orders/:id/deliver
+// @access  Private/Admin
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id)
+
+  if (order) {
+    order.isDelivered = true
+    order.deliveredAt = Date.now()
 
     const updatedOrder = await order.save()
 
     res.json(updatedOrder)
   } else {
-    res.status(404);
-    throw new Error("Order not found");
+    res.status(404)
+    throw new Error('Order not found')
   }
-});
+})
 
 // @desc    Get logged in user orders
 // @route   GET /api/orders/myorders
 // @access  Private
 const getMyOrders = asyncHandler(async (req, res) => {
-  const orders = await (await Order.find({user: req.user._id}))
+  const orders = await Order.find({ user: req.user._id })
   res.json(orders)
-});
+})
 
-export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders };
+// @desc    Get all orders
+// @route   GET /api/orders
+// @access  Private/Admin
+const getOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({}).populate('user', 'id name')
+  res.json(orders)
+})
+
+export {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+  updateOrderToDelivered,
+  getMyOrders,
+  getOrders,
+}
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -13,6 +13,9 @@ import {
   USER_UPDATE_PROFILE_REQUEST,
   USER_UPDATE_PROFILE_SUCCESS,
   USER_DETAILS_RESET,
+  USER_LIST_REQUEST,
+  USER_LIST_SUCCESS,
+  USER_LIST_FAIL,
 } from "../constants/userConstants";
 
 export const userLoginReducer = (state = {}, action) => {
@@ -69,3 +72,17 @@ export const userUpdateProfileReducer = (state = {}, action) => {
       return state;
   }
 };
+
+export const userListeReducer = (state = {users: []}, action) => {
+  switch (action.type) {
+    case USER_LIST_REQUEST:
+      return { loading: true };
+    case USER_LIST_SUCCESS:
+      return { loading: false, users: action.payload };
+    case USER_LIST_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
